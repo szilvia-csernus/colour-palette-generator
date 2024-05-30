ae3927f6ddf59c03c2f4252595832c13d1f57c1f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-05-30 15:23:48 +0100

    update prompt message contents

--- app.py
+++ app.py
@@ -13,22 +13,34 @@ app = Flask(__name__,
 
 
 def get_colors(input_text):
-    prompt = f"""
+    system_prompt = f"""
     You are a color palette generating assistant that responds to text prompts for color palettes
     You should generate color palettes that fit the theme, mood or instructions in the prompt. The palettes should be between 2 and 8 colors.
     Desired Format: a JSON array of hexadecimal color codes. Don't include json formatting. Include # in front of the hex codes.
-    
-    Q: Convert the following description of a color palette into a list of colors: Ocean tones
-    A: ["#0077be", "#4f819d", "#a8c4d0", "#61a0a8", "#2b6d84"]
-    
-    Q: Convert the following description of a color palette into a list of colors: {input_text}
-    A:
     """
+    user_prompt1 = "Ocean tones"
+    assistant_answer = ["#0077be", "#4f819d", "#a8c4d0", "#61a0a8", "#2b6d84"]
+    
+    user_prompt2 = {input_text}
+    
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user",
-            "content": f"{prompt}"
+            {
+              "role": "system",
+              "content": system_prompt
+            },
+            {
+              "role": "user",
+              "content": user_prompt1
+            },
+            {
+              "role": "assistant",
+              "content": assistant_answer
+            },
+            {
+              "role": "user",
+              "content": user_prompt2
             }
         ]
     )
